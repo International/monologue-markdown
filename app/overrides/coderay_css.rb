da20fc37e34e707afd7c02559aa42388545924ce
George Opritescu <ssscripting@gmail.com>
2013-09-12 01:21:52 +0300

    Properly precompile asset

--- app/overrides/coderay_css.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => "layouts/monologue/application",
-                     :name => "insert_coderay_css",
-                     :insert_bottom => "head",
-                     :partial => "monologue-markdown/overrides/coderay_css")
