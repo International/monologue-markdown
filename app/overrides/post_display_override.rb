f9942bf0d9cf3d24dca24384ee3d6f01c76ea81a
George Opritescu <ssscripting@gmail.com>
2013-09-12 00:02:15 +0300

    Works

--- app/overrides/post_display_override.rb
+++ app/overrides/post_display_override.rb
@@ -1,4 +1,5 @@
-Deface::Override.new(:virtual_path => "monologue/posts/post",
+Deface::Override.new(:virtual_path => "monologue/posts/_post",
                      :name => "change_post_display_mode",
-                     :replace => "div.content",
+                     :replace => "div#content",
+                     # :sequence => {:after => "monologue/posts/index" },
                      :partial => "monologue-markdown/overrides/change_post_display_mode")
